e88089d840c8fc244ae0f7e0360c5ccc0941e3f0
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-08 12:54:42 +0530

    testing orm models
    defining orm models - base.py, thread.py, user.py which will eventually connect to postgres db

---
+++ models/thread.py
@@ -0,0 +1,25 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
+
+class Thread(Base):
+    __tablename__ = "threads"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user : Mapped["User"] = relationship("User")
+
+# User is a class/dataType
+# user is an object of type User
+# users is the table name
+# user_id is the foreign key of table threads linked to table users col id
+# every row in table users is an object of type User
+
+
---
+++ models/user.py
@@ -0,0 +1,29 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from __future__ import annotations 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .thread import Thread
+
+class User(Base):
+    __tablename__ = "users"
+    id : Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    threads: Mapped[list["Thread"]] = relationship("Thread")
+
+# Thread is a class/dataType
+# thread is an object of type User
+# threads is the table name
+# every row in table threads is an object of type Thread
+
+# importing Thread in this file from .thread and importing User in .thread file from this file
+# problem of circular imports (infinite loop)
+
+# from __future__ import annotations 
+# tells python "Don't evaluate type hints immediately. Treat them as strings until later."
+
+# At runtime:
+# TYPE_CHECKING == False
+# so the import never happens, avoiding the circular import.
+# But editors like VS Code pretend it's True while doing type analysis
