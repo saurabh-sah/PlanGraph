0b1e5948e4c10dc184595b5c40936321941a53eb
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-08 23:53:49 +0530

    fixed bugs
    from __future__ import annotations must be the first import of any file which performs lazy imports; str(PostgresDsn) as create_engine parameters wants the database_url in str not in PostgresDsn; but pydantic checks the database_url to a proper postgres url and does not allow any str to fill it; hence database_url: PostgresDsn is important

--- backend/app/db/engine.py
+++ backend/app/db/engine.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from app.core.config import settings
 
 engine = create_engine(
-    settings.database_url,
+    str(settings.database_url),
     echo=True,
     pool_pre_ping=True
 )
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,6 @@
+from __future__ import annotations 
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from __future__ import annotations 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
