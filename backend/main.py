0fa849381b833f481b62d60b8331683f44cb9ad3
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-09 01:35:03 +0530

    added create_thread_route in main.py

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,10 @@ import app.models
 from app.models.base import Base
 from app.db.engine import engine
 
+from sqlalchemy.orm import Session
+from app.db.sessions import get_db
+from app.services.ThreadService import create_thread, ThreadCreateDTO
+
 Base.metadata.create_all(engine)
 
 app = FastAPI()
@@ -24,9 +28,14 @@ class ChatResponse(BaseModel):
     model: str
     success: bool
 
-class ThreadResponse(BaseModel):
-    thread_id: int
+class ThreadCreateRequest(BaseModel):
     title: str
+    user_id: int
+
+class ThreadResponse(BaseModel):
+    id: int
+    title: str
+    user_id: int
 
 class ThreadQuery(BaseModel):
     limit: int = Field(default=10, ge=1, lt=100)
@@ -45,12 +54,6 @@ def get_health():
 @app.post("/chat", response_model=ChatResponse)
 def chat_response(request: ChatRequest):
     return ChatResponse(answer=f"You said: {request.message}", model="fake-llm-v1", success=True)
-
-@app.get("/threads/{thread_id}", response_model=ThreadResponse)
-def get_thread(thread_id: int):
-    if (thread_id not in fake_threads):
-        raise HTTPException(status_code=404, detail="Thread Not Found")
-    return ThreadResponse(thread_id=thread_id, title=fake_threads[thread_id])
 
 @app.get("/threads", response_model=list[ThreadResponse], )
 def get_threads(thread_info: ThreadQuery = Depends()):
@@ -77,3 +80,14 @@ def get_threads(thread_info: ThreadQuery = Depends()):
     if not response:
         raise HTTPException(status_code=404, detail="Threads Not Found")
     return response
+
+@app.post("/threads", response_model=ThreadResponse)
+def create_thread_route(thread_info: ThreadCreateRequest):
+    db: Session = Depends(get_db)
+    thread = create_thread(
+        db=db,
+        thread_info=ThreadCreateDTO(**thread_info.__dict__)
+    )
+    return ThreadResponse(id=thread.id, title=thread.title, user_id=thread.user_id)
+
+
