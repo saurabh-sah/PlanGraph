4c2ec3ad632499cb3a119283c3db2f5a81291232
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-09 18:35:16 +0530

    added get_thread_route()

--- backend/app/routes/thread.py
+++ backend/app/routes/thread.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import Session
 from backend.app.db.session import get_db
 from backend.app.services.thread_service import create_thread
 from services.thread_service import list_threads
+from app.services.thread_service import (
+    get_thread as get_thread_service
+)
 
 router = APIRouter(
     prefix="/threads",
@@ -38,6 +41,29 @@ def create_thread_route(
         db=db,
         thread_info=thread_info,
     )
+
+    return ThreadResponse(
+        id=thread.id,
+        title=thread.title,
+        user_id=thread.user_id,
+    )
+
+@router.get("/{thread_id}", response_model=ThreadResponse)
+def get_thread_route(
+    thread_id: int,
+    db: Session = Depends(get_db),
+):
+
+    thread = get_thread_service(
+        db=db,
+        thread_id=thread_id,
+    )
+
+    if thread is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Thread does not exist",
+        )
 
     return ThreadResponse(
         id=thread.id,
