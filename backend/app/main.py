bb1fb655c5f6101dcaeee3e9868b2b703399ff43
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-11 07:33:04 +0530

    added alembic
    removed create_all() from main.py; deleted all tables; made the alembic initial migration

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,14 +3,12 @@ from .schemas import *
 from fastapi import FastAPI
 
 from . import models
-from .db.engine import engine
-from .models.base import Base
+from app.db.engine import engine
+from app.models.base import Base
 
-from .routes import *
+from app.routes import *
 
 app = FastAPI()
-
-Base.metadata.create_all(engine)
 
 app.include_router(thread_router)
 app.include_router(chat_router)
