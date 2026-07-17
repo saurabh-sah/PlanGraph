e33b8a1c30fc89d7b32eb22f2797d355b4da3cbf
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-17 19:25:19 +0530

    llm integration in planner.py

--- backend/app/graph/nodes/planner.py
+++ backend/app/graph/nodes/planner.py
@@ -1,7 +1,30 @@
 from app.graph.state import GraphState
+from app.models.enums import AgentType
+
+from langchain_core.messages import (
+    SystemMessage,
+    HumanMessage
+)
+
+from app.graph.state import (
+    GraphState
+)
 
 from app.graph.planner.schemas import (
     PlannerOutput
+)
+
+from app.graph.planner.prompts import (
+    PLANNER_SYSTEM_PROMPT
+)
+
+from app.core.llm_factory import (
+    create_llm
+)
+
+from app.graph.planner.schemas import (
+    PlannerOutput,
+    TaskSpec
 )
 
 
@@ -9,11 +32,66 @@ def planner_node(
     state: GraphState,
 ):
 
-    plan = PlannerOutput(
-        tasks=[],
-        reasoning="Runtime skeleton planner.",
+    messages = state.get(
+        "messages",
+        []
+    )
+
+    planner_context = state.get(
+        "planner_context",
+        ""
+    )
+
+    llm = create_llm()
+
+    structured_llm = (
+        llm.with_structured_output(
+            PlannerOutput
+        )
+    )
+
+    history = "\n".join(
+        [
+            f"{m.type}: {m.content}"
+            for m in messages
+        ]
+    )
+
+    if not history:
+
+        history = (
+            "User: Hello"
+        )
+        
+
+    user_prompt = f"""
+Conversation:
+
+{history}
+
+
+Retrieved Context:
+
+{planner_context}
+"""
+
+    output = structured_llm.invoke(
+
+        [
+
+            SystemMessage(
+                content=
+                    PLANNER_SYSTEM_PROMPT
+            ),
+
+            HumanMessage(
+                content=user_prompt
+            )
+        ]
     )
 
     return {
-        "planner_output": plan
+
+        "planner_output":
+            output
     }
