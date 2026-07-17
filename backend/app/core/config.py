948594118f3df7a447c7ee65cd7eecb70580d555
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-17 16:19:30 +0530

    add graph_context_window
    graph_context_window: int = 30

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class Settings(BaseSettings):
     database_url: PostgresDsn
+    graph_context_window: int = 30
     model_config = SettingsConfigDict(
         env_file=BASE_DIR /".env",
         extra="ignore"
