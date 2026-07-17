8890a58c8644b5b5211c966e286f61a845ad2533
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-17 16:22:38 +0530

    route /chat update: test_graph

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -1,15 +1,69 @@
-from app.schemas.chat import ChatRequest, ChatResponse
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+
+from app.db.session import SessionLocal
+
+from app.graph.builder import (
+    get_graph
+)
+
+from app.schemas.chat import (
+    ChatRequest,
+    ChatResponse
+)
 
 router = APIRouter(
     prefix="/chat",
     tags=["Chat"]
 )
 
-@router.post("/", response_model=ChatResponse)
-def chat_response(request: ChatRequest):
-    return ChatResponse(
-        answer=f"You said: {request.message}",
-        model="fake-llm-v1",
-        success=True,
-    )
+
+@router.post(
+    "/",
+    response_model=ChatResponse
+)
+def chat_response(
+    request: ChatRequest
+):
+
+    db: Session = SessionLocal()
+
+    try:
+
+        graph = get_graph()
+
+        result = graph.invoke(
+
+            {
+
+                "user_id": 1,
+
+                "thread_id":
+                    request.thread_id,
+
+                "trigger_message_id": 1,
+
+                "agent_run_id": 1,
+            },
+
+            config={
+
+                "configurable": {
+
+                    "db": db
+                }
+            }
+        )
+
+        return ChatResponse(
+
+            answer=str(result),
+
+            model="runtime-test",
+
+            success=True,
+        )
+
+    finally:
+
+        db.close()
