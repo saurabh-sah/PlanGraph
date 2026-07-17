e762437842ca87154800683b337558fb8f277b4e
Saurabh Kumar <saurabh.kumar.cd.che24@itbhu.ac.in>
2026-07-17 19:25:01 +0530

    persistance works

--- backend/app/graph/nodes/persist_plan.py
+++ backend/app/graph/nodes/persist_plan.py
@@ -1,11 +1,30 @@
-from app.graph.state import GraphState
+from app.services.task_service import (
+    persist_plan
+)
 
 
 def persist_plan_node(
-    state: GraphState,
+    state,
     config,
 ):
 
+    db = config["configurable"]["db"]
+
+    _, task_runs = persist_plan(
+
+        db=db,
+
+        agent_run_id=state["agent_run_id"],
+
+        plan=state["planner_output"],
+    )
+
+    db.commit()
+
     return {
-        "ready_task_run_ids": []
+
+        "ready_task_run_ids": [
+            tr.id
+            for tr in task_runs
+        ]
     }
